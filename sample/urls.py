ea2310680acea97c5722355b66cc95be94bad7f5
venuchilukala <venuchilukala07@gmail.com>
2024-09-12 11:46:13 +0530

    extend & include

--- sample/urls.py
+++ sample/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home,result
 
 urlpatterns =[
-    path('',Home)
+    path('',Home),
+    path('result',result)
 ]
--- sample/views.py
+++ sample/views.py
@@ -4,4 +4,36 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Home(request):
-    return render(request,'index.html')
+    founder = "John Wesley"
+    address= "Kolkata"
+    data = {
+        "name" : founder,
+        "add" : address
+    }
+    return render(request,'index.html',data)
+
+def result(request):
+    num1 = int(request.POST['n1'])
+    num2 = int(request.POST['n2'])
+    operator = request.POST['operator']
+    if(operator == "+"):
+        result = num1 + num2
+    elif(operator == "-"):
+        result = num1 - num2
+    elif(operator == "*"):
+        result = num1 * num2
+    elif(operator == "/"):
+        result = num1 / num2
+    elif(operator == "**"):
+        result = num1 ** num2
+    elif(operator == "%"):
+        result = num1 % num2
+    
+    
+    data2 = {
+        "num1" : num1,
+        "num2" : num2,
+        "result" : result
+    }
+    return render(request, 'result.html', data2)
+
